fbae8bbd2e216e8be926b3ba916dbeb938abaf34
George Thomas <tgeorgetom@gmail.com>
2018-10-08 13:52:15 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-$(document).foundation();
+$(document).foundation()
 
 jQuery(function($) { 
 
@@ -7,42 +7,35 @@ jQuery(function($) {
   var $slide = 'li'; // could also use 'img' if you're not using a ul
   var $transition_time = 1000; // 1 second
   var $time_between_slides = 1000; // 4 seconds
-  var count = 1; 
 
   function slides(){
     return $slider.find($slide);
   }
 
-  slides().animate({opacity: 0}, 500);
+  slides().fadeOut();
 
   // set active classes
   slides().first().addClass('active');
-  slides().first().animate({opacity: 1}, $transition_time);
+  slides().first().fadeIn($transition_time);
 
   // auto scroll 
   $interval = setInterval(
     function(){
       var $i = $slider.find($slide + '.active').index();
-      
 
       slides().eq($i).removeClass('active');
-      slides().eq($i).animate({opacity: 0}, $transition_time);
+      slides().eq($i).fadeOut($transition_time);
 
       if (slides().length == $i + 1) $i = -1; // loop to start
 
-      slides().eq($i + 1).animate({opacity: 1}, $transition_time);
+      slides().eq($i + 1).fadeIn($transition_time);
       slides().eq($i + 1).addClass('active');
-      count++;
-
-      if(slides().length == count){
-        clearInterval($interval);
-      }
-
     }
     , $transition_time +  $time_between_slides 
   );
+
+  setTimeout(function(){
+	clearInterval($interval);
+  }, 8000)
+
 });
-
-
-
-
